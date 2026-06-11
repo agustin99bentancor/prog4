d54a22bead29aba5029c15c098fd7d9d30e4e4a0
Brandon Calimares <brandoncalimares@gmail.com>
2026-06-11 19:29:24 -0300

    cambio menor en menu

--- lab4/src/Menu.cpp
+++ lab4/src/Menu.cpp
@@ -103,10 +103,6 @@ void Menu::altaUsuario() {
                 std::cout << "¿Desea agregar otra libreta? (1: Si, 0: No): ";
                 std::cin >> agregarLibreta;
                 std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-                if (std::cin.fail()) {
-                    std::cin.clear();
-                    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-                }
             } else {
                 std::cout << "Se ha alcanzado el limite maximo de libretas.\n";
             }
